14ee8700bb8b6b2d92431e3641737ed9f288a311
RamsesH1s <tanianery7@gmail.com>
2023-11-29 19:26:41 -0300

    vai se fuder

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+/*========SCROll SECTIONS AVTIVE LINK========*/
+let sections = document.querySelectorAll('section');
+let navLinks = domcument.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let heigth = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset &&  top < offset + heigth ) {
+            navLinks.forEach(links => {
+                 links.classList.remove('active');
+                 document.querySelector('header nav a[href*='+ id + ']').classList.add('active');   
+
+            });
+        };
+    });
+};
